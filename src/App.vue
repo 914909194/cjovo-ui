e453c2138ef9836c7326e808fe10e16bbe6c312e
914909194 <914909194@qq.com>
2023-03-30 21:53:40 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
       <cj-button plain type="info">info按钮</cj-button>
       <cj-button plain type="danger">danger按钮</cj-button>
       <cj-button plain type="warning">warning按钮</cj-button>
+      <cj-button plain type="warning">warning按钮</cj-button>
+
     </div>
   </div>
 </template>
